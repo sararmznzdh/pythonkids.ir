e49244a933a97d09a651611fde3541d46280fd35
S.Ramezanzadeh <76613649+sararmznzdh@users.noreply.github.com>
2024-10-02 21:37:42 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-// تابع برای اسکرول نرم به بخش‌های مختلف
-function smoothScroll(target) {
-    const element = document.querySelector(target);
-    element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-    });
-}
 
-// اضافه کردن رویداد کلیک به منو
-document.querySelectorAll('nav ul li a').forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href');
-        smoothScroll(targetId);
-    });
-});
-
-// پیام خوش‌آمدگویی
-window.onload = function() {
-    alert("به PythonKids.ir خوش آمدید! از یادگیری پایتون لذت ببرید.");
-};
 
 
 
